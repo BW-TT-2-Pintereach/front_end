b6ec42076569d04b328e87b878f83a0ef7062295
joeywire <61367542+joeywire@users.noreply.github.com>
2020-11-15 16:40:51 -0600

    Added basic global styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import ArticleDash from './components/ArticleDash'; 
+import GlobalStyle from './components/GlobalStyle'; 
 
 function App() {
   return (
     <Router>
       <div className="App">
         <h1>Pintereach</h1>
+        <GlobalStyle />
         <Route exact path="/" component={ArticleDash} />
       </div>
     </Router>
